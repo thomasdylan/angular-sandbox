0263a5b26c9e90acafd6dedc8c64bdfaabe1535c
Dylan Thomas <thomasdylan76@gmail.com>
2020-03-29 16:14:46 -0600

    ngModel & 2 Way Data Binding

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -9,10 +9,20 @@ import { User } from '../../models/User';
 })
 
 export class UsersComponent implements OnInit {
+  user: User = {
+    firstName: '',
+    lastName: '',
+    age: null,
+    address: {
+      street: '',
+      city: '',
+      state: ''
+    }
+  }
   users: User[];
   showExtended: boolean = true;
   loaded: boolean = false;
-  enableAdd: boolean = true;
+  enableAdd: boolean = false;
   currentClasses = {};
   currentStyles = {};
   showUserForm: boolean = false;
@@ -65,8 +75,22 @@ export class UsersComponent implements OnInit {
     this.loaded = true;
   }
 
-  addUser(user: User) {
-    this.users.push(user);
+  addUser() {
+    this.user.isActive = true;
+    this.user.registered = new Date();
+
+    this.users.unshift(this.user);
+
+    this.user = {
+      firstName: '',
+      lastName: '',
+      age: null,
+      address: {
+        street: '',
+        city: '',
+        state: ''
+      }
+    };
   }
 
   // toggleHide(user: User) {
