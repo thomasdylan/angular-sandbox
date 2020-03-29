30a629be119278dafe711fb055007975570b7bf5
Dylan Thomas <thomasdylan76@gmail.com>
2020-03-29 14:51:56 -0600

    Component & Template Clean Up

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -29,9 +29,7 @@ export class UsersComponent implements OnInit {
           city: 'Boston',
           state: 'MA'
         },
-        image: 'http://lorempixel.com/600/600/people/2',
         isActive: true,
-        balance: 100,
         registered: new Date('01/02/2018 08:30:00')
       },
       {
@@ -43,9 +41,7 @@ export class UsersComponent implements OnInit {
           city: 'Lynn',
           state: 'MA'
         },
-        image: 'http://lorempixel.com/600/600/people/3',
         isActive: false,
-        balance: 200,
         registered: new Date('03/11/2017 06:20:00')
       },
       {
@@ -57,41 +53,15 @@ export class UsersComponent implements OnInit {
           city: 'Miami',
           state: 'FL'
         },
-        image: 'http://lorempixel.com/600/600/people/1',
         isActive: true,
-        balance: 50,
         registered: new Date('11/02/2016 10:30:00')
       }
     ];
 
     this.loaded = true;
-
-
-    // this.addUser({
-    //   firstName: 'David',
-    //   lastName: 'Jackson'
-    // })
-
-    this.setCurrentClasses();
-    this.setCurrentStyles();
   }
 
   addUser(user: User) {
     this.users.push(user);
   }
-
-  setCurrentClasses() {
-    this.currentClasses = {
-      'btn btn-success': this.enableAdd,
-      'big-text': this.showExtended
-    };
-  }
-
-  setCurrentStyles() {
-    this.currentStyles = {
-      'padding-top': this.showExtended ? '0' : '40px',
-      'font-size': this.showExtended ? '' : '40px'
-    }
-  }
-
 }
